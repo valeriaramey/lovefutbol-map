d1e7078c8b8cdc14b56b8206a93859b13db5035d
Valeria Ramey <valeriaramey@gmail.com>
2020-11-28 23:33:43 -0500

    sets up mapbox APIX, creates markers for a few test addresses and and sets up json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMapGl, {Marker} from "react-map-gl"
+import fieldData from "./data/fields.json"
+
+
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    latitude: 40.68313619450496,
+    longitude:-73.98128935812457,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10
+  });
+
+
   return (
-    <div className="App">
-      love.futbol map
-    </div>
+   <div>
+    <ReactMapGl
+      {...viewport}
+      mapboxApiAccessToken="pk.eyJ1IjoidmFscmFtIiwiYSI6ImNrZGl6MmVzdjA5dzcycWxqZTBleGRsdGEifQ.UB5yez5jsQVEkrMajr2mog"
+      mapStyle="mapbox://styles/valram/ckf2tl39x01hi19jwu9e8alzp"
+      onViewportChange={viewport => {
+        setViewport(viewport);
+     }}
+     >
+      {fieldData.map((field) => (
+        <Marker
+          key={field.field_id}
+          latitude={field.coordinates[0]}
+          longitude={field.coordinates[1]}
+        >
+        <button class="marker-btn">
+          <img src= "/ball.png" alt="field icon" />
+        </button>
+        </Marker>
+      ))}
+    </ReactMapGl>
+   </div>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const fieldList = [
+  {
+    "name": "field at Bergen",
+    "field_id": 1,
+    "address": "328 Bergen Street",
+    "coordinates": [40.68307924267538, -73.9813966465522]
+  },
+  {
+    "name": "field at Bensonhurts",
+    "field_id": 2,
+    "address": "1938 78th street",
+    "coordinates": [40.61022768033515, -73.99460334655424]
+  }
+
+
+
+]
